799b77842b07cf650bd50cea6dcccea42c0c0889
Axel <axel.furlan95@gmail.com>
2018-05-07 15:46:43 -0300

    first commit

---
+++ SimulacionExtracciones/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacionExtracciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int extrac;
+            Console.WriteLine("Ingrese número de extraccionistas: ");
+            extrac = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
