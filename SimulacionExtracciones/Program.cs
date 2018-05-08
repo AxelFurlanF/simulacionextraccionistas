6e81b13db7f89a0fd4f564da368768cc6e663651
Axel <axel.furlan95@gmail.com>
2018-05-08 09:06:09 -0300

    ahora genera randoms diferentes

--- SimulacionExtracciones/Program.cs
+++ SimulacionExtracciones/Program.cs
@@ -9,11 +9,13 @@ namespace SimulacionExtracciones
     class Program
     {
         private const int hv = 100000000;
-       
+        
+
         static void Main(string[] args)
         {
             int e, t, tpll, ns, tf, currentTps, nt, ss, sll, sta;
-            
+            Random rnd = new Random();
+
             Console.WriteLine("Ingrese número de extraccionistas: ");
             e = Convert.ToInt32(Console.ReadLine());
 
@@ -28,18 +30,19 @@ namespace SimulacionExtracciones
             }
          
 
-            t = tpll = 0; tf = 180; ns = nt = 0; ss = sll = sta = 0;
+            t = tpll = 0; tf = 20000; ns = nt = 0; ss = sll = sta = 0;
 
             while (t <= tf)
             {
-                simulacion(e, out t, ref tpll, ref ns, out currentTps, ref nt, ref ss, ref sll, ref sta, tps, ito, sto);
+                simulacion(e, out t, ref tpll, ref ns, out currentTps, ref nt, ref ss, ref sll, ref sta, tps, ito, sto,  rnd);
             }
             while (ns > 0)
             {
                 tpll = hv;
-                simulacion(e, out t, ref tpll, ref ns, out currentTps, ref nt, ref ss, ref sll, ref sta, tps, ito, sto);
+                simulacion(e, out t, ref tpll, ref ns, out currentTps, ref nt, ref ss, ref sll, ref sta, tps, ito, sto,  rnd);
             }
 
+            Console.WriteLine("Personas totales: " + nt);
             Console.WriteLine("PEC: "+ (ss-sll-sta)/nt);
             Console.WriteLine("PTOs: ");
             int promedio=0;
@@ -55,7 +58,7 @@ namespace SimulacionExtracciones
 
         }
 
-        private static void simulacion(int e, out int t, ref int tpll, ref int ns, out int currentTps, ref int nt, ref int ss, ref int sll, ref int sta, int[] tps, int[] ito, int[] sto)
+        private static void simulacion(int e, out int t, ref int tpll, ref int ns, out int currentTps, ref int nt, ref int ss, ref int sll, ref int sta, int[] tps, int[] ito, int[] sto, Random rnd)
         {
             currentTps = encontrarMenorTps(tps);
             //proximo evento
@@ -67,7 +70,7 @@ namespace SimulacionExtracciones
                 ns--;
                 if (ns >= e)
                 {
-                    int ta = generarTA();
+                    int ta = generarTA(rnd);
                     tps[currentTps] = t + ta;
                     sta += ta;
                 }
@@ -83,14 +86,14 @@ namespace SimulacionExtracciones
             //entrada
             {
                 t = tpll;
-                tpll = t + generarIA();
+                tpll = t + generarIA(rnd);
                 ns++;
                 sll += t;
                 if (ns <= e)
                 {
                     currentTps = buscarPuesto(tps);
                     sto[currentTps] += t - ito[currentTps];
-                    int ta = generarTA();
+                    int ta = generarTA(rnd);
                     tps[currentTps] = t + ta;
                     sta += ta;
 
@@ -98,18 +101,18 @@ namespace SimulacionExtracciones
             }
         }
 
-        private static int generarIA()
+        private static int generarIA(Random rnd)
         {
-            Random rnd = new Random();
+            
             double x = rnd.NextDouble() * (0.975 - 0.01) + 0.01;
             int r = Convert.ToInt32(0.163 / Math.Pow( (-1 + Math.Pow(x, (-0.0269811))) , 0.8375209380234506));
             Console.WriteLine("IA generado: " + r);
             return r;
         }
 
-        private static int generarTA()
+        private static int generarTA(Random rnd)
         {
-            Random rnd = new Random();
+            
             double x = rnd.NextDouble() * (0.998 - 0.001) + 0.001;
             int r = Convert.ToInt32(Math.Pow (Math.Pow(1-x,-1.1886)-1 , 0.1961 * 2.1947));
             Console.WriteLine("TA generado: " + r);
